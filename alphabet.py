a8e2d43671475cf02b1969abd4e7b6dcf99fab75
Kozubi <marcink00@gmail.com>
2015-12-28 11:11:10 +0100

    Added: - Added -removing,widgets - added - sound abstraction - fixed popup behaviour

--- alphabet.py
+++ alphabet.py
@@ -39,28 +39,48 @@ class Alpha(GridLayout):
 
 
     def addWidgets(self, *args):
+        self.WIDGET_LIST = [] # to store widget list
         self.RANDOM_NUMBER = random.choice(string.ascii_uppercase)
 
         #ModalView showing stuff to choose
         self.pop = ModalView(size_hint = (.5,.5))
         self.pop.auto_dismiss = False
-        self.pop.add_widget(Label(text=str(self.RANDOM_NUMBER)))
+        self.pop.add_widget(Button(text=str(self.RANDOM_NUMBER), on_press=self.pop.dismiss))
 
         for i in string.ascii_uppercase:
-            self.add_widget(MyButton(text=str(i), on_press=self.pop.open))
+            btn = MyButton(text=str(i), on_press=self.checker)
+            self.add_widget(btn)
+            self.WIDGET_LIST.append(btn)
 
-        Clock.schedule_interval(self.openP, 3) # TODO remove it when sound will be added
+        Clock.schedule_once(self.openP, 1) # TODO remove it when sound will be added
+
 
     def openP(self, *args):
         self.pop.open()
 
-    def checker(self, *args):
+
+    def checker(self, btn):
+        print("children", self.children)
         # will check is answer is correct and draw another popup
-        if self.RANDOM_NUMBER ==
+        if self.RANDOM_NUMBER == btn.text:
+            print("WOHOOOO")
+            self.s = self.SoundPlayer("hurra.wav")
+        else:
+            print("NOOOPE")
+            self.s = self.SoundPlayer("nie.wav")
+        for i in self.WIDGET_LIST:
+            self.remove_widget(i)
+        self.addWidgets()
+
+    def SoundPlayer(self, sound):
+        s = SoundLoader()
+        s = s.load(sound)
+        s.play()
 
 
 class MyApp(App):
     Window.fullscreen=False
+    Window.size = (400,240)
     Window.title = "Apka"
     def build(self):
         return Alpha()
